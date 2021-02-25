be35e93242cc89bd3d44884952f82c25ee5de3d2
Rafael Breno <rafael_breno@outlook.com>
2021-02-24 23:30:54 -0300

    Remove unreachable code

--- cache.go
+++ cache.go
@@ -25,25 +25,17 @@ func Put(key, value string) (Cache, error) {
 		value: value,
 	}
 
-	if err := c.SetPath(); err != nil {
-		return Cache{}, err
-	}
+	c.SetPath()
 
 	return c, nil
 }
 
-func (c *Cache) SetPath() error {
+func (c *Cache) SetPath() {
 	keyBytes := sha1.Sum([]byte(c.key))
-
-	if len(keyBytes) == 0 {
-		return fmt.Errorf("Error converting key to sha1 bytes array")
-	}
 
 	c.Path = fmt.Sprintf("/%x/%x/", keyBytes[0], keyBytes[1])
 
 	for i := 0; i < len(keyBytes); i++ {
 		c.Path += fmt.Sprintf("%x", keyBytes[i])
 	}
-
-	return nil
 }
