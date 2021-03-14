03cf93d754dedff5228e709c551b2d8a4243d0a2
Rafael Breno <rafael_breno@outlook.com>
2021-03-14 18:41:37 -0300

    Add redis tests

--- tests/redis_test.go
+++ tests/redis_test.go
@@ -84,3 +84,179 @@ func Test_Redis_Put(t *testing.T) {
 		})
 	}
 }
+
+func getStoreRedisGetTests() []testRedis {
+	var t []testRedis
+
+	_, err := cache.Store(2)
+	t = append(t, testRedis{
+		name: "Incorrect Type",
+		want: fmt.Errorf("The format isn't supported"),
+		got:  err,
+	})
+
+	redisCache, _ := cache.Store(stores.Redis{})
+	_, err = redisCache.Get("")
+
+	t = append(t, testRedis{
+		name: "Key missing",
+		want: fmt.Errorf("'key' must not be nil"),
+		got:  err,
+	})
+
+	_, err = redisCache.Get("foo")
+
+	t = append(t, testRedis{
+		name: "Value Found",
+		want: nil,
+		got:  err,
+	})
+
+	_, err = redisCache.Get("bar")
+	t = append(t, testRedis{
+		name: "Value not found",
+		want: nil,
+		got:  err,
+	})
+	return t
+}
+
+func Test_Redis_Get(t *testing.T) {
+	tts := getStoreRedisGetTests()
+
+	for _, tt := range tts {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.want != nil {
+				if tt.want.Error() != tt.got.Error() {
+					t.Errorf("\nWant: %v\n Got: %v\n", tt.want, tt.got)
+				}
+			}
+		})
+	}
+}
+
+func getStoreRedisHasTests() []testRedis {
+	var t []testRedis
+
+	redisCache, _ := cache.Store(stores.Redis{})
+	_, err := redisCache.Has("")
+	t = append(t, testRedis{
+		name: "Key missing",
+		want: fmt.Errorf("'key' must not be nil"),
+		got:  err,
+	})
+
+	_, err = redisCache.Has("foo")
+
+	t = append(t, testRedis{
+		name: "Value Found",
+		want: nil,
+		got:  err,
+	})
+
+	_, err = redisCache.Has("bar")
+	t = append(t, testRedis{
+		name: "Value not found",
+		want: nil,
+		got:  err,
+	})
+	return t
+}
+
+func Test_Redis_Has(t *testing.T) {
+	tts := getStoreRedisGetTests()
+
+	for _, tt := range tts {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.want != nil {
+				if tt.want.Error() != tt.got.Error() {
+					t.Errorf("\nWant: %v\n Got: %v\n", tt.want, tt.got)
+				}
+			}
+		})
+	}
+}
+
+func getStoreRedisPullTests() []testRedis {
+	var t []testRedis
+
+	redisCache, _ := cache.Store(stores.Redis{})
+	_, err := redisCache.Has("")
+	t = append(t, testRedis{
+		name: "Key missing",
+		want: fmt.Errorf("'key' must not be nil"),
+		got:  err,
+	})
+
+	_, err = redisCache.Has("foo")
+
+	t = append(t, testRedis{
+		name: "Value Found",
+		want: nil,
+		got:  err,
+	})
+
+	_, err = redisCache.Has("bar")
+	t = append(t, testRedis{
+		name: "Value not found",
+		want: nil,
+		got:  err,
+	})
+	return t
+}
+
+func Test_Redis_Pull(t *testing.T) {
+	tts := getStoreRedisPullTests()
+
+	for _, tt := range tts {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.want != nil {
+				if tt.want.Error() != tt.got.Error() {
+					t.Errorf("\nWant: %v\n Got: %v\n", tt.want, tt.got)
+				}
+			}
+		})
+	}
+}
+
+func getStoreRedisDeleteTests() []testRedis {
+	var t []testRedis
+
+	redisCache, _ := cache.Store(stores.Redis{})
+	err := redisCache.Delete("")
+	t = append(t, testRedis{
+		name: "Key missing",
+		want: fmt.Errorf("'key' must not be nil"),
+		got:  err,
+	})
+
+	err = redisCache.Delete("foo")
+
+	t = append(t, testRedis{
+		name: "Value Found",
+		want: nil,
+		got:  err,
+	})
+
+	err = redisCache.Delete("bar")
+	t = append(t, testRedis{
+		name: "Value not found",
+		want: fmt.Errorf(""),
+		got:  err,
+	})
+	return t
+}
+
+func Test_Redis_Delete(t *testing.T) {
+	tts := getStoreRedisDeleteTests()
+
+	for _, tt := range tts {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.want != nil {
+				if tt.want.Error() != tt.got.Error() {
+					t.Errorf("\nWant: %v\n Got: %v\n", tt.want, tt.got)
+				}
+			}
+		})
+	}
+}
