7e749f1f8d3d94ca22af29fd89060024f81b00e0
Rafael Breno <rafael_breno@outlook.com>
2021-03-18 20:48:37 -0300

    Implement PkgError into stores

--- stores/stores.go
+++ stores/stores.go
@@ -1,34 +1,38 @@
 package stores
 
+import (
+	"github.com/rafaelbreno/go-cache/pkg_error"
+)
+
 type CacheInterface interface {
-	Put() error
-	Get() (string, error)
-	Has() (bool, error)
-	Delete() error
-	Pull() (string, error)
+	Put() pkg_error.PkgError
+	Get() (string, pkg_error.PkgError)
+	Has() (bool, pkg_error.PkgError)
+	Delete() pkg_error.PkgError
+	Pull() (string, pkg_error.PkgError)
 }
 
 // Put a value
-func Put(c CacheInterface) error {
+func Put(c CacheInterface) pkg_error.PkgError {
 	return c.Put()
 }
 
 // Retrieve a cached Value
-func Get(c CacheInterface) (string, error) {
+func Get(c CacheInterface) (string, pkg_error.PkgError) {
 	return c.Get()
 }
 
 // Check is key exists
-func Has(c CacheInterface) (bool, error) {
+func Has(c CacheInterface) (bool, pkg_error.PkgError) {
 	return c.Has()
 }
 
 // Check is key exists
-func Delete(c CacheInterface) error {
+func Delete(c CacheInterface) pkg_error.PkgError {
 	return c.Delete()
 }
 
 // Retrieve and delete value
-func Pull(c CacheInterface) (string, error) {
+func Pull(c CacheInterface) (string, pkg_error.PkgError) {
 	return c.Pull()
 }
