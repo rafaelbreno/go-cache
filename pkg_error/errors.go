f3c6342deb0e723dd1a79185b3476d6efe8f80a9
Rafael Breno <rafael_breno@outlook.com>
2021-03-18 21:18:43 -0300

    Add error message

--- pkg_error/errors.go
+++ pkg_error/errors.go
@@ -3,4 +3,5 @@ package pkg_error
 const (
 	FieldMustNotBeNull = "The field '%s' must not be null"
 	InvalidFormat      = "The format '%s' isn't supported"
+	CacheDontExists    = "The key '%s' doesn't have a value"
 )
