28e664774fd12447f7c374714dcb0627d3b829ff
Rafael Breno <rafael_breno@outlook.com>
2021-03-18 22:16:28 -0300

    Implement pkg_error into redis store

--- stores/redis.go
+++ stores/redis.go
@@ -2,9 +2,9 @@ package stores
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/rafaelbreno/go-cache/pkg_error"
 )
 
 // TODO: implement expiration
@@ -25,95 +25,110 @@ func SetRedisConn(r *redis.Client) {
 }
 
 // Method to store a string value into a key
-func (f *Redis) Put() error {
+func (f *Redis) Put() pkg_error.PkgError {
 	// Validate key
 	if f.Key == "" {
-		return fmt.Errorf("'key' must not be nil")
+		return pkg_error.
+			NewError(nil).
+			SetMessage(pkg_error.FieldMustNotBeNull, "key")
 	}
 
 	// Validate value
 	if len(f.Value) == 0 {
-		return fmt.Errorf("'value' must not be nil")
+		return pkg_error.
+			NewError(nil).
+			SetMessage(pkg_error.FieldMustNotBeNull, "value")
 	}
 
-	return nil
+	return pkg_error.NewNilError()
 }
 
 // Retrieve cached value
-func (r *Redis) Get() (string, error) {
+func (r *Redis) Get() (string, pkg_error.PkgError) {
 	// Validate key
 	if r.Key == "" {
-		return "", fmt.Errorf("'key' must not be nil")
+		return "", pkg_error.
+			NewError(nil).
+			SetMessage(pkg_error.FieldMustNotBeNull, "key")
 	}
 
-	var has bool
-	var err error
-
-	if has, err = r.Has(); !has {
+	if has, err := r.Has(); !has {
 		return "", err
 	}
 
-	return r.strCMD.String(), nil
+	return r.strCMD.String(), pkg_error.NewNilError()
 }
 
 // Check if Cache already exists
-func (r *Redis) Has() (bool, error) {
+func (r *Redis) Has() (bool, pkg_error.PkgError) {
 	// Validate key
 	if r.Key == "" {
-		return false, fmt.Errorf("'key' must not be nil")
+		return false, pkg_error.
+			NewError(nil).
+			SetMessage(pkg_error.FieldMustNotBeNull, "key")
 	}
 
-	// Redis `GET key` command. It returns redis.Nil error when key does not exist.
+	// Redis `GET key` command. It returns redis.Nil pkg_error.PkgError when key does not exist.
 	strCmd := redisClient.Get(ctx, r.Key)
 
 	if err := strCmd.Err(); err != redis.Nil {
-		return false, err
+		return false, pkg_error.
+			NewError(err).
+			SetMessage(pkg_error.FieldMustNotBeNull, "key")
 	}
 
 	r.strCMD = strCmd
 
-	return true, nil
+	return true, pkg_error.NewNilError()
 }
 
 // Delete cached file
-func (r *Redis) Delete() error {
+func (r *Redis) Delete() pkg_error.PkgError {
 	// Validate key
 	if r.Key == "" {
-		return fmt.Errorf("'key' must not be nil")
+		return pkg_error.
+			NewError(nil).
+			SetMessage(pkg_error.FieldMustNotBeNull, "key")
 	}
 
 	err := redisClient.Del(ctx, r.Key).Err()
 
-	return err
+	return pkg_error.
+		NewError(err)
 }
 
 // Retrieve and delete cached file
-func (r *Redis) Pull() (string, error) {
+func (r *Redis) Pull() (string, pkg_error.PkgError) {
 	// Validate key
 	if r.Key == "" {
-		return "", fmt.Errorf("'key' must not be nil")
+		return "", pkg_error.
+			NewError(nil).
+			SetMessage(pkg_error.FieldMustNotBeNull, "key")
 	}
 
 	val, err := r.Get()
 
-	if err != nil {
+	if !err.Nil {
 		return "", err
 	}
 
 	err = r.Delete()
 
-	if err != nil {
+	if !err.Nil {
 		return "", err
 	}
 
-	return val, nil
+	return val, pkg_error.NewNilError()
 }
 
 // Save cache's value into a file
-func (r *Redis) Save() error {
+func (r *Redis) Save() pkg_error.PkgError {
 	// Dumping bytes into a file
 
-	err := redisClient.Set(ctx, r.Key, r.Value, 0).Err()
+	err := redisClient.
+		Set(ctx, r.Key, r.Value, 0).
+		Err()
 
-	return err
+	return pkg_error.
+		NewError(err)
 }
