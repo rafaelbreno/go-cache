8d8b0e761b82d8ea38c4b0af2b029dcc6c259638
Rafael Breno <rafael_breno@outlook.com>
2021-04-13 19:16:45 -0300

    fix: Fixed GetType helper, some types can be an interface

--- helpers/helper.go
+++ helpers/helper.go
@@ -2,6 +2,10 @@ package helpers
 
 import "reflect"
 
+// TODO: investigate https://stackoverflow.com/questions/61217817/why-does-reflecting-the-name-or-package-path-of-the-error-type-cause-a-panic-i
 func GetType(v interface{}) string {
+	if v == nil {
+		return "nil"
+	}
 	return reflect.TypeOf(v).String()
 }
